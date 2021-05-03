10aef4f28ccecc84a48df21bd153c6faebeaaabf
hashly <ilya12-141998@mail.ru>
2021-05-03 18:39:25 +0300

    add is.. to..

--- libft.h
+++ libft.h
@@ -18,5 +18,12 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(char *s1, char *s2, size_t n);
 int		ft_atoi(char *str);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
