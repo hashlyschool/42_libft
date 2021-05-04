5cd1bc1e2a55f5c3c325c29bac581b7b7156ea3c
hashly <ilya12-141998@mail.ru>
2021-05-04 23:01:53 +0300

    add substr strjoin strtrim

--- libft.h
+++ libft.h
@@ -28,4 +28,8 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *src);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+
 #endif
