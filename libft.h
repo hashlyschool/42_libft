61d34a9639f8096c94de644d0b55673cb4033656
hashly <ilya12-141998@mail.ru>
2021-05-04 23:04:08 +0300

    add many func

--- libft.h
+++ libft.h
@@ -1,7 +1,8 @@
 #ifndef     LIBFT_H
 # define    LIBFT_H
 
-# include   <stddef.h>
+# include <stddef.h>
+# include <stdlib.h>
 
 void	*ft_memset(void *buf, int ch, size_t count);
 void	ft_bzero(void *s, size_t n);
@@ -18,5 +19,17 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(char *s1, char *s2, size_t n);
 int		ft_atoi(char *str);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *src);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
