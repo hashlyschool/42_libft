ad10a4f68c52feaf0f6524a164d9b1dc637f753a
hashlyschool <ilya12-141998@mail.ru>
2022-03-21 00:24:46 +0300

    add ft_atoi_long_long

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: hashly <hashly@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 17:21:33 by hashly            #+#    #+#             */
-/*   Updated: 2022/03/14 14:02:42 by hashly           ###   ########.fr       */
+/*   Updated: 2022/03/21 00:23:29 by hashly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,60 +23,61 @@ typedef struct s_list
 	struct s_list	*next;
 }				t_list;
 
-void	*ft_memset(void *buf, int ch, size_t count);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t count);
-void	*ft_memccpy(void *dest, const void *source, int ch, size_t count);
-void	*ft_memmove(void *dest, const void *source, size_t count);
-void	*ft_memchr(const void *arr, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-size_t	ft_strlen( const char *str );
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *str);
-int		ft_isdigit(int c);
-int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strdup(const char *src);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strjoin_free_s1(char const *s1, char const *s2);
-char	*ft_strjoin_free_s2(char const *s1, char const *s2);
-char	*ft_strjoin_free_all(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
+void		*ft_memset(void *buf, int ch, size_t count);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t count);
+void		*ft_memccpy(void *dest, const void *source, int ch, size_t count);
+void		*ft_memmove(void *dest, const void *source, size_t count);
+void		*ft_memchr(const void *arr, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t		ft_strlen( const char *str );
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_atoi(const char *str);
+long long	ft_atoi_long_long(const char *str);
+int			ft_isdigit(int c);
+int			ft_isalpha(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+void		*ft_calloc(size_t nmemb, size_t size);
+char		*ft_strdup(const char *src);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strjoin_free_s1(char const *s1, char const *s2);
+char		*ft_strjoin_free_s2(char const *s1, char const *s2);
+char		*ft_strjoin_free_all(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
+char		**ft_split(char const *s, char c);
+char		*ft_itoa(int n);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
 
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
-void	ft_free_str_of_str(char ***arg);
-char	**ft_add_line(char **arg, char *line);
-char	**ft_add_line_front(char **arg, char *line);
-void	ft_print_str_of_str(char **arr);
-size_t	ft_len_str_str(char **arr);
-char	**ft_split_2(char const *s, char *c);
-char	*ft_charjoin_libft(char const *s1, char const s2);
+void		ft_free_str_of_str(char ***arg);
+char		**ft_add_line(char **arg, char *line);
+char		**ft_add_line_front(char **arg, char *line);
+void		ft_print_str_of_str(char **arr);
+size_t		ft_len_str_str(char **arr);
+char		**ft_split_2(char const *s, char *c);
+char		*ft_charjoin_libft(char const *s1, char const s2);
 
 #endif
--- mandatory/ft_atoi.c
+++ mandatory/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: hashly <hashly@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 17:21:40 by hashly            #+#    #+#             */
-/*   Updated: 2022/01/07 17:21:43 by hashly           ###   ########.fr       */
+/*   Updated: 2022/03/21 00:22:57 by hashly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,34 @@ int	ft_atoi(const char *str)
 			return (-1);
 		else if (len_num > 12 && flag == -1)
 			return (0);
+	}
+	return (n * flag);
+}
+
+long long int	ft_atoi_long_long(const char *str)
+{
+	long long int	n;
+	int				flag;
+	size_t			len_num;
+
+	n = 0;
+	flag = 1;
+	len_num = 0;
+	while (*str == '\t' || *str == '\n' || *str == '\v' || *str == '\f'
+		|| *str == '\r' || *str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str++ == '-')
+			flag *= -1;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		if ((n == 922337203685477580 && flag == 1 && (*str - '0') > 7) || \
+		(n == 922337203685477580 && flag == -1 && (*str - '0') > 8) || \
+		(n > 922337203685477580))
+			return (2);
+		n = n * 10 + (*str++ - '0');
 	}
 	return (n * flag);
 }
